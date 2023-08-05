5fede75d965fbc86feeb7be2a0669e647d93e68b
v2rg <xzcawerw@gmail.com>
2023-08-05 17:42:53 +0500

    IndexTestCase (added)

--- products_app/tests.py
+++ products_app/tests.py
@@ -1,37 +1,38 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 # Create your tests here.
-from products_app.models import ProcessorList, VideoCardList, MotherboardList, MemoryList
 
 
 class IndexTestCase(TestCase):
-    # def setUp(self):  # создаем тестовую запись в таблице
-    #     ProcessorList.objects.create(
-    #         sku='555',
-    #         category_id=1,
-    #         brand_id=61,
-    #         name='test_processor_name',
-    #         description='test_processor_description',
-    #         short_description='test_processor_short_description',
-    #         thumbnail='static/img/test-photo.png',
-    #         price='555',
-    #         quantity='555',
-    #         active=False,
-    #         line_id=29,
-    #         socket_id=7,
-    #         cores='555',
-    #         base_frequency='555',
-    #         max_frequency='555',
-    #         memory_type_id=12,
-    #         tdp='555'
-    #     )
-    #     print('Processor test created')
+    fixtures = ['categories.json', 'brands.json', 'sockets.json', 'memory_types.json', 'cpu_lines.json',
+                'gpu_pci_versions.json', 'gpu_models.json', 'mb_form_factors.json', 'mb_chipsets.json',
+                'products_images.json', 'list_rams.json', 'list_videocards.json', 'list_motherboards.json',
+                'list_processors.json']
 
-    def test_index_view(self):
-        path = reverse('index')
-        response = self.client.get(path)
+    def setUp(self) -> None:
+        self.path = reverse('index')
+        self.response = self.client.get(self.path)
 
-        self.assertEqual(response.status_code, 200)
-        # print(response.)
-        # self.assertEqual(response., 'qwe')
+    def test_index_template(self):
+        self.assertTemplateUsed(self.response, 'products_app/index.html')
+        self.assertEqual(self.response.status_code, 200, 'Статус код НЕ 200')
+
+    def test_index_title(self):
+        self.assertEqual(self.response.context_data['title'], 'e-Store - Главная')
+
+    def test_index_queryset(self):
+        self.assertEqual(len(self.response.context_data['random_products']), 8, 'len queryset < 8')
+        self.assertEqual(len([x for x in self.response.context_data['random_products'] if x.category_id == 1]), 2,
+                         'len queryset Processor < 2')
+        self.assertEqual(len([x for x in self.response.context_data['random_products'] if x.category_id == 2]), 2,
+                         'len queryset Videocard < 2')
+        self.assertEqual(len([x for x in self.response.context_data['random_products'] if x.category_id == 3]), 2,
+                         'len queryset Motherboard < 2')
+        self.assertEqual(len([x for x in self.response.context_data['random_products'] if x.category_id == 4]), 2,
+                         'len queryset Ram < 2')
+
+
+class CatalogTestCase(TestCase):
+    ...
